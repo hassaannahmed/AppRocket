d73de01b977e005166a8350ccf6c35244750bc64
Hassaan Ahmed <hassaannahmed@gmail.com>
2020-09-05 00:41:54 +0500

    Added CORS

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const socketio = require('socket.io');
 const http = require('http');
 const path = require('path');
 const connectDB = require('./config/db');
+const cors = require('cors');
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 connectDB();
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
